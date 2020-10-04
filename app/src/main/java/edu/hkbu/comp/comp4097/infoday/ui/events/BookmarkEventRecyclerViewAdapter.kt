fdbfedf91d8a3b870dd89e1d0f4409dd2302770a
xiaxichen <xiaxichen@xiaxichens-MacBook-Air.local>
2020-10-04 22:49:44 +0800

    Lab3_S3  bookmark event swipleft(delete)

--- app/src/main/java/edu/hkbu/comp/comp4097/infoday/ui/events/BookmarkEventRecyclerViewAdapter.kt
+++ app/src/main/java/edu/hkbu/comp/comp4097/infoday/ui/events/BookmarkEventRecyclerViewAdapter.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.ItemTouchHelper
 import edu.hkbu.comp.comp4097.infoday.R
+import edu.hkbu.comp.comp4097.infoday.data.AppDatabase
 import edu.hkbu.comp.comp4097.infoday.data.Event
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 /**
@@ -30,6 +35,32 @@ class BookmarkEventRecyclerViewAdapter(
   }
 
   override fun getItemCount(): Int = values.size
+
+
+
+  override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+    ItemTouchHelper(myCallback()).attachToRecyclerView(recyclerView)
+    super.onAttachedToRecyclerView(recyclerView)
+  }
+  inner class myCallback : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+    override fun onMove(
+      recyclerView: RecyclerView,
+      viewHolder: RecyclerView.ViewHolder,
+      target: RecyclerView.ViewHolder
+    ): Boolean { return false }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+      val holder = viewHolder as ViewHolder
+      //assume your idView contains deptID and contentView contains event's title
+      val unbookmarkItem = values.find { it.deptId+ ":" + it.id == holder.deptIdView.text}
+      unbookmarkItem?.bookmarked = false
+      if (unbookmarkItem != null)
+        CoroutineScope(Dispatchers.IO).launch {
+          AppDatabase.getInstance(viewHolder.itemView.context).
+          eventDao().update(unbookmarkItem)
+        }
+    }
+  }
 
   inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val deptIdView: TextView = view.findViewById(R.id.item_number)
